3ce130cc6ea212b99725338acfe8455499c63789
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2020-03-21 07:57:12 +0100

    make sure that the path belongs to root inside the container.

--- initMount.sh
+++ initMount.sh
@@ -13,12 +13,15 @@ relativepath=${localpath:1}
 containerpath=/var/lib/lxd/containers/$containername/rootfs/$relativepath
 mountname="${relativepath//\//_}"
 
-mkdir -p $hostpath
-chmod -R a+rwx $hostpath
 rm -Rf $containerpath
 mkdir -p $containerpath
 
 # see https://stgraber.org/2017/06/15/custom-user-mappings-in-lxd-containers/
 # you will only be able to write to the mounted directory, if it allows writing for other.
 # it will be owned by user and group nobody, uid 65534
+# see https://discuss.linuxcontainers.org/t/mount-a-folder-in-host-into-lxd-container-nogroup-nobody-in-the-container/6341
+mkdir -p $hostpath
+chmod -R a+rwx $hostpath
+chown -R 1000000.1000000 $hostpath
+
 lxc config device add $containername $mountname disk source=$hostpath/ path=$localpath
