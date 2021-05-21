7f51aaf929fafdc7b0d9cea0fd8e81c2ba1f53c1
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-05-21 23:04:00 +0200

    improve performance of listcontainers.sh

--- listcontainers.sh
+++ listcontainers.sh
@@ -13,6 +13,8 @@ then
   show=$1
 fi
 
+lxclist=`lxc list`
+
 echo "container path: $container_path"
 tmpfile=/tmp/listcontainers.txt
 echo "--" > $tmpfile
@@ -32,21 +34,22 @@ do
   # version=getOSOfContainer
   getOSOfContainer $rootfs
 
-  if [[ -z "`lxc list $name | grep RUNNING`" ]]
+  if [[ -z "`echo "$lxclist" | grep $name | grep RUNNING`" ]]
   then
     state="stopped"
   else
     state="running"
   fi
 
-  if [[ "true" == "`lxc config get $name boot.autostart`" ]]
+  #if [[ "true" == "`lxc config get $name boot.autostart`" ]]
+  autostart=`cat $d/backup.yaml | grep 'boot.autostart: "true"'`
+  if [[ ! -z "$autostart" ]]
   then
     autostart="yes"
   else
     autostart="no"
   fi
 
-  IPv4=`lxc list $name -c 4 --format csv`
   IPv4=`cat $lxdbr0_path/dnsmasq.hosts/$name | awk -F"," '{print $2}'`
 
   if [[ "$show" == "all" || "$show" == "$state" ]]
